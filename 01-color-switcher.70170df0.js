7312bc228cf5e128f3d0cb4cbdca39f30cfc9ed9
VovaKyrychuk <112816108+VovaKyrychuk@users.noreply.github.com>
2023-02-28 08:04:45 +0000

    Deploying to gh-pages from @ VovaKyrychuk/goit-js-hw-09@3caffa5caec989b193c0b646974ce8a17f45e5a5 🚀

--- 01-color-switcher.70170df0.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o;function c(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;n.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,o=setInterval(c,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.70170df0.js.map
