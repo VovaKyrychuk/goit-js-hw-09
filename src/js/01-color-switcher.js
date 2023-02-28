3caffa5caec989b193c0b646974ce8a17f45e5a5
VovaKyrychuk <kyrychuk81@gmail.com>
2023-02-28 10:04:16 +0200

    Urgrade Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,34 +1,53 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-const body = document.querySelector('body');
 
-let intervalId;
+// const body = document.querySelector('body');
 
-// Функція, яка генерує випадковий колір
+// let intervalId;
+
+// // Генеруємо випадковий колір
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// // Функція, яка змінює фоновий колір елемента <body> на випадкове значення
+// function changeBackgroundColor() {
+//   const randomColor = getRandomHexColor();
+//   body.style.backgroundColor = randomColor;
+// }
+
+// // Функція, яка запускає інтервал зміни кольору
+// function startBackgroundColorChange() {
+//   // Встановлюємо властивість "disabled" кнопки "Start"
+//   startBtn.disabled = true;
+//   // Запускаємо інтервал зміни кольору
+//   intervalId = setInterval(changeBackgroundColor, 1000);
+// }
+
+// // Функція, яка зупиняє інтервал зміни кольору
+// function stopBackgroundColorChange() {
+//   startBtn.disabled = false;
+//   // Зупиняємо інтервал зміни кольору
+//   clearInterval(intervalId);
+// }
+
+// startBtn.addEventListener('click', startBackgroundColorChange);
+// stopBtn.addEventListener('click', stopBackgroundColorChange);
+
+let intervalId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Функція, яка змінює фоновий колір елемента <body> на випадкове значення
-function changeBackgroundColor() {
-  const randomColor = getRandomHexColor();
-  body.style.backgroundColor = randomColor;
-}
-
-// Функція, яка запускає інтервал зміни кольору
-function startBackgroundColorChange() {
-  // Встановлюємо властивість "disabled" кнопки "Start"
+startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
-  // Запускаємо інтервал зміни кольору
-  intervalId = setInterval(changeBackgroundColor, 1000);
-}
-
-// Функція, яка зупиняє інтервал зміни кольору
-function stopBackgroundColorChange() {
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+stopBtn.addEventListener('click', () => {
   startBtn.disabled = false;
-  // Зупиняємо інтервал зміни кольору
+  stopBtn.disabled = true;
   clearInterval(intervalId);
-}
-
-startBtn.addEventListener('click', startBackgroundColorChange);
-stopBtn.addEventListener('click', stopBackgroundColorChange);
+});
